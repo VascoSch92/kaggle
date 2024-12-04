aacf8643969b45f564ee24b59a06aa6ec9061fb4
vasco <vasco.schiavo@protonmail.com>
2024-12-04 16:47:41 +0100

    49670349(insurance --light-lgbm-regressor)

--- insurance/models/light_lgbm_regressor.py
+++ insurance/models/light_lgbm_regressor.py
@@ -1,55 +1,35 @@
 import warnings
+from pathlib import Path
 from collections import namedtuple
 
+import numpy as np
 import optuna
 from lightgbm import LGBMRegressor
 from sklearn.metrics import root_mean_squared_log_error
+from sklearn.model_selection import cross_val_score
+
+from tools.load import load_parameters
+from tools.save import save_parameters
 
 warnings.filterwarnings("ignore")
 
 
 def train_light_lgbm_regressor(params: namedtuple) -> LGBMRegressor:
-    params.logger.info("Starting LGBMClassifier Training")
+    params.logger.info("Starting LGBMRegressor Training")
 
-    def objective(trial):
-        param_grid = {
-            "metric": "neg_mean_squared_log_error",
-            "boosting_type": trial.suggest_categorical("boosting_type", ["gbdt", "dart"]),
-            "num_leaves": trial.suggest_int("num_leaves", 20, 150),
-            "learning_rate": trial.suggest_loguniform("learning_rate", 0.01, 0.3),
-            "feature_fraction": trial.suggest_uniform("feature_fraction", 0.5, 1.0),
-            "bagging_fraction": trial.suggest_uniform("bagging_fraction", 0.5, 1.0),
-            "bagging_freq": trial.suggest_int("bagging_freq", 1, 10),
-            "min_child_samples": trial.suggest_int("min_child_samples", 5, 50),
-            "lambda_l1": trial.suggest_loguniform("lambda_l1", 1e-8, 10.0),
-            "lambda_l2": trial.suggest_loguniform("lambda_l2", 1e-8, 10.0),
-            "random_state": params.config.random_state,
-            "verbose": -1,
-            "num_threads": -1,  # Use a single thread,
-        }
+    params_path = Path(params.config.paths.data) / "light_lgbm_regressor.json"
 
-        model = LGBMRegressor(**param_grid)
-        model.fit(
-            params.X_train,
-            params.y_train,
-            eval_set=[(params.X_val, params.y_val)],
-            eval_names=["validation"],
-            eval_metric="neg_mean_squared_log_error",
-        )
+    if params_path.exists():
+        best_params = load_parameters(filepath=params_path, logger=params.logger)
+    else:
+        params.logger.info("Start study with Optuna")
 
-        y_train = model.predict(params.X_train)
-        y_val_pred = model.predict(params.X_val)
+        func = lambda trial: objective(trial, params)
+        study = optuna.create_study(direction="minimize", study_name="LGBMRegressor")
+        study.optimize(func, n_trials=20)
+        best_params = study.best_params
 
-        train_score = root_mean_squared_log_error(y_train, params.y_train)
-        val_score = root_mean_squared_log_error(y_val_pred, params.y_val)
-
-        return (train_score + val_score) / 2.0
-
-    params.logger.info("Start study with Optuna")
-    study = optuna.create_study(direction="minimize", study_name="LGBMRegressor")
-    study.optimize(objective, n_trials=20)
-
-    best_model = LGBMRegressor(**study.best_params)
+    best_model = LGBMRegressor(**best_params)
     best_model.fit(
         params.X_train,
         params.y_train,
@@ -58,10 +38,41 @@ def train_light_lgbm_regressor(params: namedtuple) -> LGBMRegressor:
         eval_metric="neg_mean_squared_log_error",
     )
 
-    params.logger.info(f"Best parameters found: {study.best_params}")
-    params.logger.info(f"Best model score: {study.best_value}")
-    # params.logger.info(
-    #     f"Best validation score: {best_model.evals_result_['validation']['neg_mean_squared_log_error'][-1]}"
-    # )
+    params.logger.info(f"Best parameters found: {best_params}")
+    params.logger.info(f"Best model score: {best_model.best_score_}")
 
+    save_parameters(path=params_path, parameters=best_params, logger=params.logger)
     return best_model
+
+
+def objective(trial, params: namedtuple) -> float:
+    param_grid = {
+        "metric": "neg_mean_squared_log_error",
+        "boosting_type": trial.suggest_categorical("boosting_type", ["gbdt", "dart"]),
+        "num_leaves": trial.suggest_int("num_leaves", 20, 150),
+        "learning_rate": trial.suggest_loguniform("learning_rate", 0.01, 0.3),
+        "feature_fraction": trial.suggest_uniform("feature_fraction", 0.5, 1.0),
+        "bagging_fraction": trial.suggest_uniform("bagging_fraction", 0.5, 1.0),
+        "bagging_freq": trial.suggest_int("bagging_freq", 1, 10),
+        "min_child_samples": trial.suggest_int("min_child_samples", 5, 50),
+        "lambda_l1": trial.suggest_loguniform("lambda_l1", 1e-8, 10.0),
+        "lambda_l2": trial.suggest_loguniform("lambda_l2", 1e-8, 10.0),
+        "random_state": params.config.random_state,
+        "verbose": -1,
+        "num_threads": -1,  # Use a single thread,
+    }
+
+    model = LGBMRegressor(**param_grid)
+    model.fit(
+        params.X_train,
+        params.y_train,
+        eval_set=[(params.X_val, params.y_val)],
+        eval_names=["validation"],
+        eval_metric="neg_mean_squared_log_error",
+    )
+
+    y_val_pred = model.predict(params.X_val)
+    val_score = root_mean_squared_log_error(y_val_pred, params.y_val)
+    scores = cross_val_score(model, params.X_train, params.y_train, cv=5, scoring="neg_mean_squared_error")
+
+    return (np.mean(np.sqrt(-1 * scores)) + val_score) / 2
--- insurance/train.py
+++ insurance/train.py
@@ -15,6 +15,7 @@ from tools.schema import Schema
 from insurance.models.xgboost_regressor import train_xgboost_regressor
 from insurance.models.catboost_regressor import train_catboost_regressor
 from insurance.models.light_lgbm_regressor import train_light_lgbm_regressor
+from insurance.models.random_forest_regressor import train_random_forest_regressor
 
 
 class InsuranceTrain(Task):
@@ -102,6 +103,8 @@ class InsuranceTrain(Task):
                 return train_catboost_regressor(params=params)
             case "--xgboost-regressor":
                 return train_xgboost_regressor(params=params)
+            case "--random-forest-regressor":
+                return train_random_forest_regressor(params=params)
             case _:
                 raise KeyError(f"Model {self.model} not found!")
 
