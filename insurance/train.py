a0ae74c55c29ed855abb3dc2d836e56aee60fec3
vasco <vasco.schiavo@protonmail.com>
2024-12-06 07:08:20 +0100

    07bf2a1d(insurance --light-lgbm --stratification)

--- insurance/train.py
+++ insurance/train.py
@@ -149,7 +149,7 @@ class InsuranceTrain(Task):
         dfs: Data,
         model: Any,
     ) -> pd.DataFrame:
-        stratified_k_fold = RepeatedKFold(n_splits=5, n_repeats=5, random_state=self.config.random_state)
+        stratified_k_fold = RepeatedKFold(n_splits=8, n_repeats=2, random_state=self.config.random_state)
         splits = stratified_k_fold.split(X, y)
 
         scores, test_predictions = [], []
