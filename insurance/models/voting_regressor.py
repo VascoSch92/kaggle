79b88108b4fdd925bdc60f8078a450f1b398589a
vasco <vasco.schiavo@protonmail.com>
2024-12-08 14:43:34 +0100

    82b877d7(insurance --voting-regressor --stratification)

--- insurance/models/voting_regressor.py
+++ insurance/models/voting_regressor.py
@@ -4,17 +4,20 @@ from sklearn.ensemble import VotingRegressor
 
 from insurance.models.catboost_regressor import train_catboost_regressor
 from insurance.models.light_lgbm_regressor import train_light_lgbm_regressor
+from insurance.models.hist_gradient_boosting_regressor import train_hist_gradient_boosting_regressor
 
 
 def train_voting_regressor(params: namedtuple) -> VotingRegressor:
     params.logger.info("Starting Booster Training")
     catboost = train_catboost_regressor(params=params)
     lightlgbm = train_light_lgbm_regressor(params=params)
+    histboost = train_hist_gradient_boosting_regressor(params=params)
     # xgboosting = train_xgboost_regressor(params=params)
     model = VotingRegressor(
         estimators=[
             ("catboost", catboost),
             ("lightlgbm", lightlgbm),
+            ("histboost", histboost),
         ],
         verbose=False,
     )
