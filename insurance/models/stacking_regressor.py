39bf2adbf78dcb91d3dc74581fdff519d61a95cc
vasco <vasco.schiavo@protonmail.com>
2024-12-09 20:30:26 +0100

    6497ed52(insurance --stacking-regressor --stratification)

--- insurance/models/stacking_regressor.py
+++ insurance/models/stacking_regressor.py
@@ -1,7 +1,6 @@
 from sklearn.ensemble import StackingRegressor
 from sklearn.linear_model import LinearRegression
 
-from insurance.models.catboost_regressor import train_catboost_regressor
 from insurance.models.light_lgbm_regressor import train_light_lgbm_regressor
 from insurance.models.hist_gradient_boosting_regressor import train_hist_gradient_boosting_regressor
 
@@ -9,11 +8,9 @@ from insurance.models.hist_gradient_boosting_regressor import train_hist_gradien
 def train_stacking_regressor(params):
     params.logger.info("Starting StackingRegressor Training")
     lightlgbm = train_light_lgbm_regressor(params=params)
-    catboost = train_catboost_regressor(params=params)
     histboost = train_hist_gradient_boosting_regressor(params=params)
     model = StackingRegressor(
         estimators=[
-            ("catboost", catboost),
             ("lightlgbm", lightlgbm),
             ("histboost", histboost),
         ],
